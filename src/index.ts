53e5c3f4f92d5c43acf8fe78201630dbb4c642bd
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-10-26 13:55:13 +0700

    implement addClass method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 export class $ {
+  private mutations: ((element: Element) => void)[] = [];
   constructor(public query: string) {}
+
+  addClass(className: string) {
+    this.mutations.push((element) => {
+      element.classList.add(className);
+    });
+    return this;
+  }
 
   *[Symbol.iterator]() {
     for (const element of document.querySelectorAll(this.query)) {
+      for (const mutation of this.mutations) {
+        mutation(element);
+      }
       yield element;
     }
   }
--- src/spec.ts
+++ src/spec.ts
@@ -9,4 +9,21 @@ describe('query', () => {
     const [div] = new $('div');
     expect(div.textContent).toBe('Hello');
   });
+
+  it('should add class', () => {
+    globalThis.document = parseHTML(/* HTML */ `
+      <div>Hello</div>
+      <div>World</div>
+    `).document;
+
+    const [div] = new $('div').addClass('foo');
+    expect(div.classList.contains('foo')).toBe(true);
+
+    // since the iterator is lazy, the second div should not have the class
+    expect(
+      Array.from(new $('div')).map((element) =>
+        Array.from(element.classList.values())
+      )
+    ).toEqual([['foo'], []]);
+  });
 });
